d7a18da51a4be2180fc1c15ce174cca0bcc52670
KaziTanvir <kazitanvir.bangladesh@gmail.com>
2022-02-07 10:51:31 +0600

    07-02-2022

---
+++ Class work/PHP/test.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "HellO world" . "...." . "Fun";
+
+    $name = "KTH";
+    $cgpa = 3.75;
+    echo $name;
+    echo $cgpa;
+
+    echo "Tanvir"."\n"."H";
+
+    $a = readline('Enter a string: ');
+ 
+    // For output
+    echo $a;
+    $a = 0;
+    for($i=0;$i<10;$i++){
+        
+        echo $i." ".  "Kazi Tanvir <br>";
+    }
+
+?>
